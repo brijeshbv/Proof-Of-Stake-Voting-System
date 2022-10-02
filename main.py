d2bdd618e55bf592c686e2228436f910ddfd62b9
patilveda <patilveda@myvuw.ac.nz>
2022-10-02 16:25:24 +1300

    Added a rest-api

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     # python run main.py <IP> <Port>
     hostIP = sys.argv[1]
     port = int(sys.argv[2])
+    apiPort = int(sys.argv[3])
  
     #initialize a node
     node = Node(hostIP = hostIP, port=port)
     node.startP2P()
+    node.startAPI(apiPort)
--- node.py
+++ node.py
@@ -1,12 +1,16 @@
 from blockchain import BlockChain
 from transaction_pool import TransactionPool
+from utils import BlockChainUtils
 from wallet import Wallet
 from socket_communication import SocketCommunication
+from node_api import NodeAPI
+from message import Message
+from utils import BlockChainUtils
 
 class Node():
 
     def __init__(self, hostIP, port) -> None:
-        self.transationPool = TransactionPool()
+        self.transactionPool = TransactionPool()
         self.wallet = Wallet()
         self.blockChain = BlockChain()
         self.hostIP = hostIP
@@ -15,4 +19,21 @@ class Node():
 
     def startP2P(self):
         self.p2p = SocketCommunication(self.hostIP, self.port)
-        self.p2p.startSocketCommunication()
+        self.p2p.startSocketCommunication(self)
+
+    def startAPI(self, apiPort):
+        self.api = NodeAPI()
+        self.api.injectNode(self)
+        self.api.start(apiPort)
+
+    def handleTransaction(self, transaction):
+        data = transaction.payload()
+        signature = transaction.signature
+        senderPublicKey = transaction.senderPublicKey
+        signatureValid = Wallet.signatureValid(data, signature, senderPublicKey)
+        transactionExists = self.transactionPool.transactionExists(transaction)
+        if not transactionExists and signatureValid:
+            self.transactionPool.addTransaction(transaction)
+            message = Message(self.p2p.socketConnector, 'TRANSACTION', transaction)
+            encodedMessage = BlockChainUtils.encode(message)
+            self.p2p.broadcast(encodedMessage)
